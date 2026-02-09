d5beb6560a47177c19b09ad4dd65139a5b9b2f2c
Sonny Ochoa <0xsonny@gmail.com>
2026-02-09 11:49:13 -0800

    Added get and set tests

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use llm_rs::tensors::Tensor;
 //use llm_rs::ops::matmul::MatMul;
 use llm_rs::tensors::flatten_matrix;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     // let a = Tensor::new(2, 3);
     // let b = Tensor::new(3, 4);
     // let c = MatMul::forward(&a, &b);
@@ -19,7 +19,20 @@ fn main() {
     println!("Shape: {:?}", my_tensor.shape);
     println!("Strides: {:?}", my_tensor.strides);
 
-    // simple assertion test
-    assert_eq!(my_tensor.strides, vec![3, 1]);
-    println!("test passed");
+    // get assertion
+    assert_eq!(my_tensor.get(&[0, 0])?, 1.1);
+    assert_eq!(my_tensor.get(&[0, 2])?, 3.3);
+    assert_eq!(my_tensor.get(&[1, 1])?, 5.5);
+    println!("\n[passed] my_tensor.get tests");
+    println!("[-test-] my_tensor.get(&[0, 0]) -> 1.1");
+    println!("[-test-] my_tensor.get(&[0, 2]) -> 3.3");
+    println!("[-test-] my_tensor.get(&[1, 1]) -> 5.5");
+
+    // set assertion
+    // tensor.set(&[1, 2], 99.0)?;
+    // assert_eq!(tensor.get(&[1, 2])?, 99.0);
+    my_tensor.set();
+    println!("\n[passed] my_tensor.set test");
+    println!("[-test-] my_tensor.get(&[1, 2]) -> 99.0");
+    Ok(())
 }
